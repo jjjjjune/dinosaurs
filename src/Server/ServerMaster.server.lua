b60a475de507d6c3478a07889249612ac5ff34a5
Matthew Little <matthewlittlethe@gmail.com>
2020-02-20 23:56:46 -0800

    add new services to servermaster

--- src/Server/ServerMaster.server.lua
+++ src/Server/ServerMaster.server.lua
@@ -1,8 +1,14 @@
 local import = require(game.ReplicatedStorage.Shared.Import)
 
 local loadOrder = {
+	"Server/Systems/PlayerWrapper",
+	"Server/Systems/CharacterWrapper",
+	"Server/Systems/RespawnManager",
 	"Server/Systems/Chemistry",
 	"Server/Systems/Carrying",
+	"Server/Systems/Classes",
+	"Server/Systems/Seasons",
+	"Server/Systems/Plants",
 }
 
 for _, path in ipairs(loadOrder) do
