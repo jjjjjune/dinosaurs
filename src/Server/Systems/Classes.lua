953bdc903a3ae6063ee34f54572aa23920e7669f
Matthew Little <matthewlittlethe@gmail.com>
2020-02-20 23:56:52 -0800

    add player classes

---
+++ src/Server/Systems/Classes.lua
@@ -0,0 +1,43 @@
+local import = require(game.ReplicatedStorage.Shared.Import)
+local Messages = import "Shared/Utils/Messages"
+local Players = game:GetService("Players")
+
+local PlayerClassesFolder = import "ServerStorage/PlayerClasses"
+
+local possibleClasses = {"Citizen", "Gatherer", "Warrior"}
+
+local function setClass(character, className)
+    local classAsset = PlayerClassesFolder[className]
+    for _, value in pairs(classAsset.Humanoid:GetChildren()) do 
+        if value:IsA("NumberValue") then 
+            character.Humanoid[value.Name].Value = value.Value
+        end
+    end
+    character.UpperTorso.BrickColor = classAsset.UpperTorso.BrickColor
+    character.RightUpperLeg.BrickColor = classAsset.RightUpperLeg.BrickColor
+    character.LeftUpperLeg.BrickColor = classAsset.LeftUpperLeg.BrickColor
+    for _, v in pairs(character:GetChildren()) do 
+        if v:IsA("Accessory") then 
+            if not v:FindFirstChild("HatAttachment", true) and not v:FindFirstChild("HairAttachment", true) then 
+                v:Destroy()
+            end
+        end
+    end
+    if character:FindFirstChild("Shirt") then
+        character.Shirt:Destroy()
+    end
+    if character:FindFirstChild("Pants") then
+        character.Pants:Destroy()
+    end
+end
+
+local Classes = {}
+
+function Classes:start()
+    Messages:hook("CharacterAdded", function(player, character)
+        wait()
+        setClass(character, possibleClasses[math.random(1, #possibleClasses)])
+    end)
+end
+
+return Classes
