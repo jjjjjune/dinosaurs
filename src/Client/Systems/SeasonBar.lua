576bc4e8dfb078aedb939f1b1e2a10a06973dca9
Matthew Little <matthewlittlethe@gmail.com>
2020-02-24 00:05:24 -0800

    Add seasons bar

---
+++ src/Client/Systems/SeasonBar.lua
@@ -0,0 +1,59 @@
+local import = require(game.ReplicatedStorage.Shared.Import)
+local Messages = import "Shared/Utils/Messages"
+local FastSpawn = import "Shared/Utils/FastSpawn"
+local SeasonsData = import "Shared/Data/SeasonsData"
+
+local TweenService = game:GetService("TweenService")
+
+local seasonRotations = {
+    310.5, -- spring start
+    221, -- summer
+    131.5, -- fall
+    41.5 -- tween to -49.5 after this
+}
+
+local function getNextSeasonRotation(seasonIndex)
+    local nextIndex = seasonIndex + 1
+    if nextIndex > #SeasonsData then
+        nextIndex = 1
+    end
+    return seasonRotations[nextIndex]
+end
+
+local function getSeasonName(seasonIndex)
+    return SeasonsData[seasonIndex].name
+end
+
+local function initializeAndHookSeasonUi()
+    local SeasonUi = game.Players.LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("StatusBar")
+    local wheel = SeasonUi.MainImage.SeasonWheel.Wheel
+    FastSpawn(function()
+        local currentSeasonIndex, timeProgressed, seasonLength = Messages:requestServer("GetSeason")
+        local alpha = timeProgressed/seasonLength
+        local startRotation = seasonRotations[currentSeasonIndex]
+        local goalRotation = getNextSeasonRotation(currentSeasonIndex)
+        local realStartRotation = (startRotation +  (goalRotation - startRotation)*alpha)
+        wheel.Rotation = realStartRotation
+        local tweenInfo = TweenInfo.new(seasonLength - timeProgressed, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+        local tween = TweenService:Create(wheel, tweenInfo, {Rotation = goalRotation})
+        tween:Play()
+    end)
+    Messages:hook("SeasonSetTo",function(seasonIndex, seasonLength)
+        wheel.Rotation = seasonRotations[seasonIndex]
+        local goalRotation = getNextSeasonRotation(seasonIndex)
+        if seasonIndex == #SeasonsData then
+            goalRotation = -49.5
+        end
+        local tweenInfo = TweenInfo.new(seasonLength, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+        local tween = TweenService:Create(wheel, tweenInfo, {Rotation = goalRotation})
+        tween:Play()
+    end)
+end
+
+local SeasonBar = {}
+
+function SeasonBar:start()
+    initializeAndHookSeasonUi()
+end
+
+return SeasonBar
