f2279f41adb25ecbeda29f6198d44d7b27692f62
Matthew Little <matthewlittlethe@gmail.com>
2020-02-26 15:58:59 -0800

    Fix up throwing

--- src/Server/Systems/Items.lua
+++ src/Server/Systems/Items.lua
@@ -5,7 +5,6 @@ local CollectionService = game:GetService("CollectionService")
 local function attemptCarryItem(player, item)
     local character = player.Character
     if not character then
-        print("no char")
         return
     end
     local foundItem = false
@@ -17,19 +16,19 @@ local function attemptCarryItem(player, item)
         end
     end
     if foundItem == true then
-        print("oh no found uitem")
         return
     end
     local alive = character:FindFirstChild("Humanoid") and character.Humanoid.Health > 0
     if not alive then 
-        print("da alive !!")
         return
     end
+    Messages:send("PlaySound", "UiClick", character.Head)
+    print("Play sound")
     character.PrimaryPart.RootPriority = 200
     item.Parent = character
     item.PrimaryPart.RootPriority = 10 -- for later when you're throwing
     item.PrimaryPart:SetNetworkOwner(player)
-    item:SetPrimaryPartCFrame(player.Character.Head.CFrame * CFrame.new(0,4,0))
+    item.PrimaryPart.CFrame = player.Character.Head.CFrame * CFrame.new(0, item.PrimaryPart.Size.Y/2 + player.Character.Head.Size.Y/2, 0)
     local serverWeld = Instance.new("WeldConstraint", item)
     serverWeld.Part0 = player.Character.Head
     serverWeld.Part1 = item.PrimaryPart
@@ -37,28 +36,53 @@ local function attemptCarryItem(player, item)
     Messages:sendClient(player, "SetCarryItem", item)
 end
 
-local function throw(item)
+local function throw(player, item)
     if item:FindFirstChild("ServerWeld") then
         item.ServerWeld:Destroy()
     end
     item.Parent = workspace
-    delay(2, function()
-        if item:IsDescendantOf(game) then 
-            item.PrimaryPart:SetNetworkOwnershipAuto()
+    Messages:send("PlaySound", "HeavyWhoosh", item.PrimaryPart)
+    delay(6, function()
+        if item:IsDescendantOf(game) and item.PrimaryPart:CanSetNetworkOwnership() then 
+            local netOwner = item.PrimaryPart:GetNetworkOwner() 
+            if netOwner == nil or netOwner == player then
+                item.PrimaryPart:SetNetworkOwnershipAuto()
+            end
         end
     end)
 end
 
+local function throwAllPlayerItems(player)
+    local character = player.Character
+    for _, p in pairs(character:GetChildren()) do
+        if CollectionService:HasTag(p, "Item") then
+                throw(player, p)
+            break
+        end
+    end
+end
+
 local Items = {}
+
+function Items.createItem(itemName, position)
+    local itemModel = game.ServerStorage.Items[itemName]:Clone()
+    itemModel.PrimaryPart = itemModel.Base
+    itemModel:SetPrimaryPartCFrame(CFrame.new(position))
+    itemModel.Parent = workspace
+    return itemModel
+end
 
 function Items:start()
     -- todo: force drop an item if a player is carrying it on leave
+    Messages:hook("CreateItem", function(itemName, position)
+        Items.createItem(itemName, position)
+    end)
     Messages:hook("PlayerDied", function(player, character)
-        for _, p in pairs(character:GetChildren()) do
-            if CollectionService:HasTag(p, "Item") then
-                    throw(p)
-                break
-            end
+        throwAllPlayerItems(player)
+    end)
+    Messages:hook("PLayerRemoving", function(player)
+        if player.Character then
+            throwAllPlayerItems(player)
         end
     end)
     Messages:hook("CarryItem", function(player, item)
@@ -66,7 +90,7 @@ function Items:start()
     end)
     Messages:hook("Throw", function(player, item)
         if item.Parent == player.Character then
-            throw(item)
+            throw(player, item)
         end
     end)
 end
