80e5f516f2911813e96659d6cf70039931c0f829
Matthew Little <matthewlittlethe@gmail.com>
2020-02-27 16:05:29 -0800

    add weather

--- src/Client/Systems/Lighting.lua
+++ src/Client/Systems/Lighting.lua
@@ -4,14 +4,31 @@ local SeasonLighting = import "Shared/Data/SeasonLighting"
 local SeasonsData = import "Shared/Data/SeasonsData"
 local TweenService = game:GetService("TweenService")
 local FastSpawn = import "Shared/Utils/FastSpawn"
+local RunService = game:GetService("RunService")
+local WeatherParticles = import "ReplicatedStorage/WeatherParticles"
+local GetCharacterPosition = import "Shared/Utils/GetCharacterPosition"
+local CastRay = import "Shared/Utils/CastRay"
+local WeatherEffectsFolder = Instance.new("Folder", workspace)
+WeatherEffectsFolder.Name = "WeatherEffects"
 
 local currentSeasonName = "Winter"
+local currentWeather = nil
+
+local lastTweens = {}
 
 local function onSeasonSetTo(currentSeason)
-    currentSeasonName = SeasonsData[currentSeason].name
+    for _, tween in pairs(lastTweens) do
+        if tween.PlaybackState == Enum.PlaybackState.Playing then
+            tween:Pause()
+        end
+    end
+    lastTweens = {}
+    currentSeasonName = (SeasonsData[currentSeason] and SeasonsData[currentSeason].name) or currentSeason
+    -- that last bit there is for weather
     local tweenInfo = TweenInfo.new(3, Enum.EasingStyle.Quad,Enum.EasingDirection.Out)
     for instance, properties in pairs(SeasonLighting[currentSeasonName]) do
         local tween = TweenService:Create(instance, tweenInfo, properties)
+        table.insert(lastTweens, tween)
         tween:Play()
     end
 end
@@ -23,6 +40,28 @@ local function setInitialLighting()
     end)
 end
 
+local function weatherStep()
+    if currentWeather ~= nil then
+        if not WeatherEffectsFolder:FindFirstChild(currentWeather) then
+            if WeatherParticles:FindFirstChild(currentWeather) then
+                local effect = WeatherParticles:FindFirstChild(currentWeather):Clone()
+                effect.Parent = WeatherEffectsFolder
+            end
+        end
+        local effect = WeatherEffectsFolder:FindFirstChild(currentWeather)
+        local pos = GetCharacterPosition()
+        local hit, hitPos = CastRay(pos, Vector3.new(0,100,0), {game.Players.LocalPlayer.Character, WeatherEffectsFolder})
+        if not hit then
+            effect.ParticleEmitter.Rate = 300
+            effect.CFrame = CFrame.new(hitPos)
+        else
+            effect.ParticleEmitter.Rate = 0
+        end
+    else
+        WeatherEffectsFolder:ClearAllChildren()
+    end
+end
+
 local Lighting = {}
 
 function Lighting:start()
@@ -32,6 +71,23 @@ function Lighting:start()
         Messages:hook("SeasonSetTo", function(currentSeason)
             onSeasonSetTo(currentSeason)
         end)
+    end)
+    Messages:hook("WeatherSetTo", function(weather)
+        if SeasonLighting[weather] then
+            onSeasonSetTo(weather)
+        else
+            local seasonNumber = 1
+            for i, seasonInfo in pairs(SeasonsData) do
+                if seasonInfo.name == currentSeasonName then
+                    seasonNumber = i
+                end
+            end
+            onSeasonSetTo(seasonNumber)
+        end
+        currentWeather = weather
+    end)
+    RunService.Stepped:connect(function()
+        weatherStep()
     end)
 end
 
---
+++ src/Server/Systems/Weather.lua
@@ -0,0 +1,35 @@
+local import = require(game.ReplicatedStorage.Shared.Import)
+local Messages = import "Shared/Utils/Messages"
+
+local currentWeather = nil
+local weatherEnd = tick() - 100
+
+local function createWeather(weatherType, length)
+    currentWeather = weatherType
+    weatherEnd = tick() + length
+    Messages:sendAllClients("WeatherSetTo", weatherType)
+    delay(length, function()
+        Messages:sendAllClients("WeatherSetTo", nil)
+    end)
+end
+
+local Weather = {}
+
+function Weather:start()
+    Messages:hook("SeasonSetTo", function(currentSeason)
+        if currentSeason == 1 then
+            createWeather("Rain", 8)
+        elseif currentSeason == 2 then
+            Messages:send("DryAllWater")
+        elseif currentSeason == 4 then
+            createWeather("Snow", 8)
+        end
+    end)
+    Messages:hook("PlayerAdded", function(player)
+        if tick() < weatherEnd then
+            Messages:sendClient(player, "WeatherSetTo", currentWeather)
+        end
+    end)
+end
+
+return Weather
