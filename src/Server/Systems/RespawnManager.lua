e848fe3422046f05b0adf0f062fa2d3c2d794d7c
Matthew Little <matthewlittlethe@gmail.com>
2020-02-27 16:05:47 -0800

    add respawn skullz

--- src/Server/Systems/RespawnManager.lua
+++ src/Server/Systems/RespawnManager.lua
@@ -1,16 +1,49 @@
 local import = require(game.ReplicatedStorage.Shared.Import)
 local Messages = import "Shared/Utils/Messages"
+local CollectionService = game:GetService("CollectionService")
 
 local RespawnManager = {}
 
 function RespawnManager:start()
     Messages:hook("PlayerDied", function(player, characterThatDied)
-        delay(5, function()
+        if characterThatDied.PrimaryPart then
+            local items = import "Server/Systems/Items"
+            local skull = items.createItem("Skull", characterThatDied.PrimaryPart.Position)
+            skull.Name = "PlayerSkull"
+            local skullPlayer = Instance.new("ObjectValue", skull)
+            skullPlayer.Name = "Player"
+            skullPlayer.Value = player
+            CollectionService:AddTag(skull, "PlayerSkull")
+            Messages:send("PlaySound", "Smoke", characterThatDied.PrimaryPart.Position)
+            Messages:send("PlayParticle", "DeathSmoke",  20, skull.PrimaryPart.Position)
+            for _, part in pairs(characterThatDied:GetChildren()) do
+                if part:IsA("BasePart") then
+                    part:Destroy()
+                end
+            end
+        else
+            -- fell off map or something?
             player:LoadCharacter()
-        end)
+        end
+    end)
+    Messages:hook("CharacterAdded", function(player, character)
+        for _, skull in pairs(CollectionService:GetTagged("PlayerSkull")) do
+            if skull.Player.Value == player then
+                Messages:send("PlaySound", "Smoke", skull.PrimaryPart.Position)
+                Messages:send("PlayParticle", "DeathSmoke",  20, skull.PrimaryPart.Position)
+                Messages:send("DestroyItem", skull)
+            end
+        end
     end)
     Messages:hook("PlayerAdded", function(player)
         player:LoadCharacter()
+    end)
+    Messages:hook("SeasonSetTo", function(currentSeason)
+        for _, p in pairs(game.Players:GetPlayers()) do
+            if (not p.Character) or (p.Character and p.Character:FindFirstChild("Humanoid") and p.Character.Humanoid.Health <= 0) then
+                p:LoadCharacter()
+            end
+        end
     end)
 end
 
