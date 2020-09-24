1f177bc0dba148c172062afb9c585ff91536277e
june <matthewlittlethe@gmail.com>
2020-09-24 16:45:27 -0700

    change common chest drops

--- src/Server/Data/ChestDrops.lua
+++ src/Server/Data/ChestDrops.lua
@@ -1,6 +1,7 @@
 return {
-	Workshop = {
-		{name = "Clay", min = 1, max = 3, chance = 45},
-		{name = "Stone", min = 0, max = 1, chance = 25},
+	["Common Chest"] = {
+		{name = "Workshop Recipe Book", min = 1, max = 3, chance = 45},
+		{name = "Cooked Meat", min = 0, max = 3, chance = 45},
+		{name = "Iron", min = 0, max = 2, chance = 45},
 	}
 }
